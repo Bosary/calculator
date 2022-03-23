4ef3dc85d413a32fae4753ba1c46b0935c99431c
Kevin <k.lassauniere@live.fr>
2022-03-23 12:11:44 +0100

    Link over-display

--- script.js
+++ script.js
@@ -36,20 +36,29 @@ function operate(operator, num1, num2) {
 
 function clear() {
     document.querySelector('.display').textContent = "00";
+    document.querySelector('.over-display').textContent = "00";
 }
 
-function display(e) {
-    let currentContent = document.querySelector('.display').textContent;
+// Change display after every change number/operator
+function displayMain(e) {
+    document.querySelector('.display').textContent = `${e.target.textContent}`;    
+}
+
+// Display the whole operation
+function displayTop(e) {
+    let currentContent = document.querySelector('.over-display').textContent;
     if (currentContent == '00') currentContent = "";
-    document.querySelector('.display').textContent = `${currentContent}${e.target.textContent}`;
+    document.querySelector('.over-display').textContent = `${currentContent}${e.target.textContent}`;
 }
 
 document.getElementById('clear').addEventListener('click', clear);
 
 const digits = document.querySelector('.digits');
-digits.addEventListener('click', display);
+digits.addEventListener('click', displayMain);
+digits.addEventListener('click', displayTop);
 
 const operators = document.querySelector('.operators');
-operators.addEventListener('click', display);
+operators.addEventListener('click', displayMain);
+operators.addEventListener('click', displayTop);
 
 // EQUAL
